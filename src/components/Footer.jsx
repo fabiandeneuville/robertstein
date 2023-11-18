108fb4ba02de0fcc155b8c63f579a17ca8e2ac43
Fabian Deneuville <deneuville.fabian@live.fr>
2023-11-18 15:49:25 +0100

    Removing email from mailto in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer(){
 
     return (
         <footer className="footer p-5 border-top border-3 d-flex flex-column justify-content-center align-items-center">
-            <a className="footer-link" href="mailto:deneuville.fabian@live.fr, info@charlielezin.com">Kontakt</a>
+            <a className="footer-link" href="mailto:deneuville.fabian@live.fr">Kontakt</a>
             <a className="footer-link" href="mailto:deneuville.fabian@live.fr">Webmaster</a>
             <p>{year}</p>
         </footer>
